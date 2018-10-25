fa89d6a3facf9d85c1c11593e43fb10b1ddc7048
Jan Andersson <jan.andersson@gmail.com>
2018-10-25 23:52:24 +0200

    Handle input in map

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,29 @@
 import React from "react"
 import { StyleSheet, StatusBar, View, TextInput } from "react-native"
 import { MapView } from "expo"
+import { decode } from "pluscodes"
+import * as R from "ramda"
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null
+  }
+
+  state = {
+    latitude: 59.332438,
+    longitude: 18.118813,
+    delta: 0.0922,
+    error: false,
+    search: ""
+  }
+
+  decode = e => {
+    const code = e.nativeEvent.text.toUpperCase()
+    const coord = decode(code)
+    this.setState({ error: !coord, search: code })
+    if (!coord) return
+    this.setState(R.pick(["latitude", "longitude"], coord))
+    this.setState({ delta: coord.resolution * 20 })
   }
 
   render() {
@@ -12,20 +31,28 @@ export default class HomeScreen extends React.Component {
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: 59.332438,
-            longitude: 18.118813,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: this.state.delta,
+            longitudeDelta: this.state.delta
           }}
           showsUserLocation={true}
           rotateEnabled={false}
-        />
+        >
+          <MapView.Marker
+            coordinate={R.pick(["latitude", "longitude"], this.state)}
+            opacity={this.state.search ? 1.0 : 0}
+          />
+        </MapView>
         <View style={styles.bubble}>
           <TextInput
+            ref={"input"}
             style={styles.input}
-            underlineColorAndroid="transparent"
+            underlineColorAndroid={this.state.error ? "red" : "transparent"}
             placeholder="Enter a plus code"
+            defaultValue={this.state.search}
+            onEndEditing={this.decode}
           />
         </View>
       </View>
@@ -38,7 +65,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     marginTop: StatusBar.currentHeight
   },
-  input: { padding: 4, fontSize: 18 },
+  input: { padding: 4, fontSize: 16 },
   map: {
     ...StyleSheet.absoluteFillObject
   },
@@ -46,7 +73,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     margin: 10,
     paddingVertical: 4,
-    paddingHorizontal: 8,
+    paddingHorizontal: 12,
     borderRadius: 10,
     elevation: 10
   }
