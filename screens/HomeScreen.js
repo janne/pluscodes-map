915c6c5e97b642a1a55f4480d4973d8b6c5fbc36
Jan Andersson <jan.andersson@gmail.com>
2018-10-25 22:49:17 +0200

    Add search box to map

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,6 @@
 import React from "react"
-import { StatusBar } from "react-native"
+import { StyleSheet, StatusBar, View, TextInput } from "react-native"
 import { MapView } from "expo"
-
-const styles = {
-  mapView: { flex: 1, marginTop: StatusBar.currentHeight }
-}
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -13,15 +9,43 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <MapView
-        style={styles.mapView}
-        initialRegion={{
-          latitude: 46.78825,
-          longitude: 18.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      />
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 43.78825,
+            longitude: 18.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
+        <View style={styles.bubble}>
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="Enter a plus code"
+          />
+        </View>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    marginTop: StatusBar.currentHeight
+  },
+  input: { padding: 4, fontSize: 18 },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  },
+  bubble: {
+    backgroundColor: "#fff",
+    margin: 10,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 10,
+    elevation: 10
+  }
+})
